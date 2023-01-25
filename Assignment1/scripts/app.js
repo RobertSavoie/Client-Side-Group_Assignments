703269c6fefc33822eaf30f2f8bf29069d804439
Robert Savoie <robert.savoie1@dcmail.ca>
2023-01-24 21:06:27 -0500

    Changed all the html to javascript. Started navbar

--- Assignment1/scripts/app.js
+++ Assignment1/scripts/app.js
@@ -12,7 +12,7 @@
                 DisplayHomePage();
                 break;
             case "Our Products":
-                DisplayProductsPage();
+                DisplayProjectsPage();
                 break;
             case "Our Services":
                 DisplayServicesPage();
@@ -28,9 +28,9 @@
     window.addEventListener("load", Start)
 
     function DisplayHomePage() {
-        let ProductsButton = document.getElementById("ProductsBtn");
-        ProductsButton.addEventListener("click", function (){
-            location.href = "products.html";
+        let ProjectsButton = document.getElementById("ProjectsBtn");
+        ProjectsButton.addEventListener("click", function (){
+            location.href = "projects.html";
         });
         let ServicesButton = document.getElementById("ServicesBtn");
         ServicesButton.addEventListener("click", function (){
@@ -40,42 +40,128 @@
         AboutUsButton.addEventListener("click", function(){
             location.href = "about.html";
         });
-        let ContactUsButton = document.getElementById("ContactUsBtn");
-        ContactUsButton.addEventListener("click", function (){
-            location.href = "contact.html"
-        });
 
-        // Paragraph creation using content injection
-        let MainContent = document.getElementsByTagName("main")[0];
-        let MainParagraph = document.createElement("p")
-        MainParagraph.setAttribute("id", "MainParagraph")
-        MainParagraph.setAttribute("class", "mt-3")
-        MainParagraph.textContent = "Welcome to Rob and Rhys' website!"
-        MainContent.appendChild(MainParagraph);
+        // Mains
+        let Main1 = document.getElementsByTagName("main")[0];
+        // Headers
+        let Header1 = document.createElement("h1")
+        // Paragraphs
+        let Paragraph1 = document.createElement("p")
 
-        // Template strings
-        // let FirstString = "This is";
-        // let SecondString = `${FirstString} the Main Paragraph.`;
-        // MainParagraph.textContent = SecondString;
+        // Welcome header
+        Header1.setAttribute("class", "mb-5")
+        Header1.textContent = "Welcome to Our Website!"
+        Main1.appendChild(Header1);
 
-        // Create article
-        // let Article = document.createElement("article");
-        // let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is my Article Paragraph</p>`;
-        // Article.setAttribute("class", "container")
-        // Article.innerHTML = ArticleParagraph;
-        // MainContent.appendChild(Article);
+        // Welcome paragraph
+        Paragraph1.setAttribute("class", "mt-3")
+        Paragraph1.textContent = "Welcome to Rob and Rhys' website!"
+        Main1.appendChild(Paragraph1);
+
     }
-    function DisplayProductsPage() {
+
+    function DisplayProjectsPage() {
         let HomeButton = document.getElementById("HomeBtn");
         HomeButton.addEventListener("click", function (){
             location.href = "index.html";
         });
+
+        // Mains
+        let Main1 = document.getElementsByTagName("main")[0];
+        // Headers
+        let Header1 = document.createElement("h1")
+        let Header2 = document.createElement("h1")
+        let Header3 = document.createElement("h1")
+        // Paragraphs
+        let Paragraph1 = document.createElement("p")
+        let Paragraph2 = document.createElement("p")
+        // Images
+        let Image1 = document.createElement("img")
+        let Image2 = document.createElement("img")
+        // Blank row
+        let Blank1 = document.createElement("br")
+        let Blank2 = document.createElement("br")
+        // Horizontal line
+        let Line1 = document.createElement("hr")
+        let Line2 = document.createElement("hr")
+
+        // Rob's Projects
+        Main1.appendChild(Paragraph1);
+        Header1.setAttribute("class", "mb-5")
+        Header1.textContent = "Rob and Rhys' Projects"
+        Paragraph1.appendChild(Header1);
+        Header2.setAttribute("class", "robh1")
+        Header2.textContent = "Rob's Projects"
+        Paragraph1.appendChild(Header2);
+        Paragraph1.appendChild(Line1);
+        Image1.setAttribute("class", "img-timetable")
+        Image1.setAttribute("src", "images/screenshot6.png")
+        Paragraph1.appendChild(Image1);
+        Paragraph1.appendChild(Blank1);
+        Paragraph1.appendChild(Blank2);
+
+        // Rhys' Projects
+        Main1.appendChild(Paragraph2);
+        Header3.setAttribute("class", "rhysh1")
+        Header3.textContent = "Rhys' Projects"
+        Paragraph2.appendChild(Header3);
+        Paragraph2.appendChild(Line2);
+        Image2.setAttribute("class", "img-timetable")
+        Image2.setAttribute("src", "images/screenshot6.png")
+        Paragraph2.appendChild(Image2);
     }
     function DisplayServicesPage() {
         let HomeButton = document.getElementById("HomeBtn");
         HomeButton.addEventListener("click", function (){
             location.href = "index.html";
         });
+
+        // Mains
+        let Main1 = document.getElementsByTagName("main")[0];
+        // Headers
+        let Header1 = document.createElement("h1")
+        let Header2 = document.createElement("h1")
+        let Header3 = document.createElement("h1")
+        // Paragraphs
+        let Paragraph1 = document.createElement("p")
+        let Paragraph2 = document.createElement("p")
+
+        // Main Header
+        Header1.setAttribute("class", "mb-5")
+        Header1.textContent = "Services we provide:"
+        Main1.appendChild(Header1);
+
+        // Rob's services
+        let RobList1 = document.createElement("li")
+        let RobList2 = document.createElement("li")
+        let RobList3 = document.createElement("li")
+        RobList1.textContent = "HTML/PHP"
+        RobList2.textContent = "C#/Visual Studio"
+        RobList3.textContent = "COBOL"
+        Header2.setAttribute("class", "robh1")
+        Header2.textContent = "Rob's Skills:"
+        Main1.appendChild(Header2);
+        Paragraph1.setAttribute("class", "mt-3")
+        Paragraph1.appendChild(RobList1)
+        Paragraph1.appendChild(RobList2)
+        Paragraph1.appendChild(RobList3)
+        Main1.appendChild(Paragraph1);
+
+        // Rhys services
+        let RhysList1 = document.createElement("li")
+        let RhysList2 = document.createElement("li")
+        let RhysList3 = document.createElement("li")
+        RhysList1.textContent = "HTML/PHP"
+        RhysList2.textContent = "C#/Visual Studio"
+        RhysList3.textContent = "COBOL"
+        Header3.setAttribute("class", "rhysh1")
+        Header3.textContent = "Rhys' Skills:"
+        Main1.appendChild(Header3);
+        Paragraph2.setAttribute("class", "mt-3")
+        Paragraph2.appendChild(RhysList1)
+        Paragraph2.appendChild(RhysList2)
+        Paragraph2.appendChild(RhysList3)
+        Main1.appendChild(Paragraph2);
     }
     function DisplayAboutUsPage() {
         let HomeButton = document.getElementById("HomeBtn");
---
+++ Assignment1/scripts/navbar.js
@@ -0,0 +1,39 @@
+// <header>
+// <nav className="navbar navbar-expand-lg">
+//     <div className="container-fluid">
+//         <a className="navbar-brand" href="index.html"><i className="fa-solid fa-hippo"></i> WEBD6201</a>
+//         <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+//                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+//                 aria-label="Toggle navigation">
+//             <span className="navbar-toggler-icon"></span>
+//         </button>
+//         <div className="collapse navbar-collapse" id="navbarSupportedContent">
+//             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+//                 <li className="nav-item">
+//                     <a className="nav-link" href="index.html"><i className="fa-solid fa-house"></i> Home</a>
+//                 </li>
+//                 <li id="projectsnav" className="nav-item">
+//                     <a className="nav-link" href="projects.html"><i className="fa-solid fa-inbox"></i> Projects</a>
+//                 </li>
+//                 <li className="nav-item">
+//                     <a className="nav-link active" aria-current="page" href="services.html"><i
+//                         className="fa-solid fa-bell-concierge"></i> Services</a>
+//                 </li>
+//                 <li className="nav-item">
+//                     <a className="nav-link" href="about.html"><i className="fa-solid fa-people-group"></i> About Us</a>
+//                 </li>
+//             </ul>
+//         </div>
+//     </div>
+// </nav>
+// <header>
+
+function DisplayNavbar(){
+    let Header = document.createElement("header")
+    let Nav = document.createElement("nav")
+    let Div1 = document.createElement("div")
+    let Div2 = document.createElement("div")
+    let A1 = document.createElement("a")
+    let Button1 = document.createElement("button")
+    let UL = document.createElement("ul")
+}
