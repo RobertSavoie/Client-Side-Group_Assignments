0d7d8514f1595cec3d31708fcea7a440ae9bd9f8
Robert Savoie <robert.savoie1@dcmail.ca>
2023-02-24 16:50:38 -0500

    Error message test commit

--- Assignment2/scripts/app.js
+++ Assignment2/scripts/app.js
@@ -304,7 +304,7 @@
      * This function displays the login page
      */
     function DisplayLoginPage(){
-        console.log("Display Login Page")
+        console.log("Display Login Page");
 
         // const urlParams = new URLSearchParams(window.location.search);
         // const urlUserName = urlParams.get('username');
@@ -451,23 +451,23 @@
     function RegisterFormValidation(){
         ValidateField("#firstName",
             /^[a-zA-Z]{2,}$/,
-            "Please enter a valid first name (ex. Peter Parker)");
+            "First name must be at least 2 characters");
         // Validate full name
         ValidateField("#lastName",
             /^[a-zA-Z]{2,}$/,
-            "Please enter a valid last name (ex. Peter Parker)");
+            "Last name must be at least 2 characters");
         // Validate Phone Number
         ValidateField("#email",
             /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,10}$/,
-            "Please enter a valid email address (ex. example@email.com");
+            "Email must be at least 8 characters with an @ symbol");
         ValidateField("#username",
             /^(?=.{6,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
-            "Characters must range from 6-20 (ex: johndoe07)");
+            "Characters must range between 6-20 characters");
         // Enter password validation
         ValidateField("#password",
             /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/,
             "Please enter a valid password. Must have a minimum of 6 characters, " +
-            "1 letter, and one special character. (ex. ");
+            "1 letter, and one special character");
     }
 
     function ValidateField(inputFieldID, regularExpression, errorMessage){
